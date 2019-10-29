c8419b01751cbc30a8b0925fd598b09dcb85043c
Mathias Battistella <mathias2b@gmail.com>
2019-10-29 16:40:58 -0300

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppComponent } from './app.component';
 
 import { MiComponente } from './components/mi-componente/mi-componente.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
-import { PruebasComponent } from './components/pruebas/pruebas.component'
+import { PruebasComponent } from './components/pruebas/pruebas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MiComponente,
     PeliculasComponent,
-    PruebasComponent
+    PruebasComponent,
+    HeaderComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule
